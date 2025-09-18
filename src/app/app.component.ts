b36ce4c1a75abf0fef65912e8ccc7658196fd06a
Denis Tsykalov <denis.tsykalov@chisw.com>
2025-09-18 14:29:38 +0200

    feature: add layoyt for task list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NbButtonModule, NbLayoutModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   imports: [
-    CoreModule
+    CoreModule,
+    SharedModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
